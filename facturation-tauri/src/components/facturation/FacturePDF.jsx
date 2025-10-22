729200f592fa9688218fe0043c1350b9c3fbe355
khalidach <khalidplaysg@gmail.com>
2025-10-22 02:01:10 +0100

    khali

--- facturation-tauri/src/components/facturation/FacturePDF.jsx
+++ facturation-tauri/src/components/facturation/FacturePDF.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useQuery } from "@tanstack/react-query";
 import { numberToWordsFr } from "@/services/numberToWords.js";
-import { invoke } from "@tauri-apps/api/tauri";
+import { invoke } from "@tauri-apps/api";
 
 const api = {
   getSettings: () => invoke("get_settings"),
